1cdd0e74e59fe6c6b32ed10e45dffc3b3bafb236
Fallen01135 <fallen01135gaming@posteo.de>
2026-03-27 19:40:42 +0100

    Update Miep

--- update_submodules.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Updating Library Submodules...
-
-git submodule update --init --recursive
-git submodule update --remote --merge
-
-echo Alle Submodule sind jetzt auf dem neuesten Stand!
-pause
